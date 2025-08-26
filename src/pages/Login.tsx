eca941bde2429070a47f971c6858da7e09595bf8
Salada <lucasvmo42@gmail.com>
2025-08-26 15:25:41 -0300

    complet

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,10 +9,10 @@ function Login() {
         />
       </div>
       <h1 className="text-white text-4xl font-bold mb-6">Login</h1>
-      <form className="bg-white  p-8 rounded-lg shadow-md w-96">
+      <form className="p-8 rounded-lg shadow-md w-96 ">
         <div className="mb-4">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-white text-sm font-bold mb-2"
             htmlFor="username"
           >
             Username
@@ -20,13 +20,13 @@ function Login() {
           <input
             type="text"
             id="username"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Enter your username"
           />
         </div>
         <div className="mb-6">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-white text-sm font-bold mb-2"
             htmlFor="password"
           >
             Password
@@ -34,7 +34,7 @@ function Login() {
           <input
             type="password"
             id="password"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Enter your password"
           />
         </div>
